f045072e2b33f1ec9cf1c7314f5b8a020af34df6
levdoescode <levdoescode+sites@gmail.com>
2023-01-19 22:20:45 -0500

    Fixed passing the correct element to showModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,14 @@
+import App from "../../app"
+import { createElement } from "../../helpers/domHelper";
+import { showModal } from "./modal"
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  let winnerNameElement = createElement({ tagName: 'div', className: 'modal-body' });
+  winnerNameElement.innerHTML = fighter.name;
+  showModal({ title: "WINNER!", bodyElement: winnerNameElement, onClose: restartGame });
+}
+
+function restartGame() {
+  root.innerHTML = ''
+  App.startApp();
 }
