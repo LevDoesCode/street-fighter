c520c263927fc60b47f9589e4d7f65adeab94c45
levdoescode <levdoescode+sites@gmail.com>
2023-01-19 19:13:52 -0500

    Implemented randIntegerRange, copied fighter stats

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,6 +1,10 @@
 import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
+  let fighter1 = {...firstFighter};
+  let fighter2 = {...secondFighter};
+  fighter1.health = 10;
+  console.log(fighter1, fighter2);
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
   });
@@ -16,4 +20,9 @@ export function getHitPower(fighter) {
 
 export function getBlockPower(fighter) {
   // return block power
+}
+
+// Function to get a random integer including bounds
+function randIntegerRange(min, max) {
+  return min + Math.round((max - min) * Math.random());
 }
