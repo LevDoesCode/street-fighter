a422ba0c61643817e9f0003ca076bd19037d884b
levdoescode <levdoescode+sites@gmail.com>
2023-01-19 21:24:51 -0500

    Implemented fight with auto fight, events pending

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,28 +1,51 @@
 import { controls } from '../../constants/controls';
 
 export async function fight(firstFighter, secondFighter) {
-  let fighter1 = {...firstFighter};
-  let fighter2 = {...secondFighter};
-  fighter1.health = 10;
-  console.log(fighter1, fighter2);
+  let fighter1 = { ...firstFighter };
+  let fighter2 = { ...secondFighter };
+  fighter1['isblocking'] = false;
+  fighter2['isblocking'] = false;
+
   return new Promise((resolve) => {
-    // resolve the promise with the winner when fight is over
+    document.addEventListener('keyup' , () => {resolve(autoFight(fighter1, fighter2, 1))});
   });
 }
 
 export function getDamage(attacker, defender) {
-  // return damage
+  return Math.max(0, getHitPower(attacker) - getBlockPower(defender));
 }
 
 export function getHitPower(fighter) {
-  // return hit power
+  let criticalHitChance = randIntegerRange(1, 2);
+  return fighter.attack * criticalHitChance;
 }
 
 export function getBlockPower(fighter) {
-  // return block power
+  let dodgeChance = randIntegerRange(1, 2);
+  return fighter.defense * dodgeChance;
 }
 
 // Function to get a random integer including bounds
 function randIntegerRange(min, max) {
   return min + Math.round((max - min) * Math.random());
+}
+
+// Function to reduce the health from a player
+function getHit(player1, player2) {
+  let damage = getDamage(player2, player1);
+  player1.health -= damage;
+}
+
+function autoFight(player1, player2, scale) {
+  player1.health = player1.health * scale;
+  player2.health = player2.health * scale;
+  while(player1.health > 0 && player2.health > 0) {
+    let rand = randIntegerRange(0, 1);
+    if (rand === 0) {
+      getHit(player1, player2)
+    } else {
+      getHit(player2, player1)
+    }
+  }
+  return player1.health <= 0 ? player2 : player1;
 }
